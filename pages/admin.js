d40c2eda2eba9ff268e5a2109197a41a473001f2
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-15 23:13:00 +0200

    Adminbar init

--- pages/admin.js
+++ pages/admin.js
@@ -1,35 +1,37 @@
 import Image from "next/image";
 
 export default function AboutPage() {
-  return (
-    <div className="grid md:grid-cols-2 gap-6 grid-cols-1">
-      <div>
-        <section className="mb-6">
-          <h2 className="mb-3 text-xl font-bold">What is Tailwind?</h2>
 
-          <p>
-            Tailwind CSS is a highly customizable, low-level CSS framework that
-            gives you all of the building blocks you need to build bespoke
-            designs without any annoying opinionated styles you have to fight to
-            override.
-          </p>
-        </section>
+    return (
+        <div className="grid md:grid-cols-2 gap-6 grid-cols-1">
+            <div>
+                <section className="mb-6">
+                    <h2 className="mb-3 text-xl font-bold">Update your status</h2>
+                </section>
 
-        <section>
-          <h2 className="mb-3 text-xl font-bold">What is Next.js?</h2>
-          <p>
-            Next.js is a minimalistic framework for creating server-rendered
-            React applications.
-          </p>
-        </section>
-      </div>
+                <section>
+                    <form>
+                        <label>
+                            What are you doing?
+                            <input className="ml-4 border-4" type="text"/>
+                        </label>
+                        <br />
+                        <label>
+                            How would you express that with an emoji?
+                            <input className="ml-4 border-4 w-8" type="text"/>
+                        </label>
+                        <br />
+                        <label>
+                            Where are you currently located?
+                            <input className="ml-4 border-4" type="text" value="Home" />
+                        </label>
+                    </form>
+                </section>
+            </div>
 
-      <Image
-        alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
-        src="/critter.svg"
-        width={476}
-        height={297.17}
-      />
-    </div>
-  );
+            <Image alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer" src="/critter.svg"
+                width={476}
+                height={297.17}/>
+        </div>
+    );
 }
