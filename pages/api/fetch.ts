613021eabd232519ed50b7e2a56c512686795a29
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-24 12:08:50 +0200

    Remove manual GET URL

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 require('dotenv').config();
 
 export default (_request: VercelRequest, response: VercelResponse) : void => {
-  const endpoint = process.env.GET_URL || "https://pepino.filiptronicek.workers.dev/";
+  const endpoint = process.env.GET_URL;
   fetch(endpoint).then(res => res.json()).then((res => {
     response.status(200).send(res);
   }))
