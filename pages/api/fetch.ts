a46f2dceb1b7be98e5ba10ff0c3154534189aa51
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-24 12:01:21 +0200

    Dotenv

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -1,8 +1,10 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
+require('dotenv').config();
+
 export default (_request: VercelRequest, response: VercelResponse) : void => {
-  const endpoint = process.env.endpoint || "https://pepino.filiptronicek.workers.dev/";
+  const endpoint = process.env.GET_URL || "https://pepino.filiptronicek.workers.dev/";
   fetch(endpoint).then(res => res.json()).then((res => {
     response.status(200).send(res);
   }))
--- pages/api/update.ts
+++ pages/api/update.ts
@@ -1,8 +1,10 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
+require('dotenv').config();
+
 export default (request: VercelRequest, response: VercelResponse) : void => {
-  const endpoint = process.env.endpoint;
+  const endpoint = process.env.SET_URL;
   fetch(`${endpoint}?location=${request.query.location}&emoji=${request.query.emoji}&title=${request.query.title}`).then(res => res.json()).then((res => {
     response.status(200).send(res);
   }))
