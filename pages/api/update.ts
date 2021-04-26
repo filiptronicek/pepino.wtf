0bde187fb67f81a330298ee84bf766fe9ed277ab
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-26 22:28:04 +0200

    TOKEN => AUTH_TOKEN

--- pages/api/update.ts
+++ pages/api/update.ts
@@ -7,12 +7,13 @@ require('dotenv').config();
 
 export default (request: VercelRequest, response: VercelResponse) : void => {
   const providedToken = request.query.token;
-  if (process.env.TOKEN === sha256(providedToken)) {
+  if (process.env.AUTH_TOKEN === sha256(providedToken)) {
     const endpoint = process.env.SET_URL;
     fetch(`${endpoint}?location=${request.query.location}&emoji=${request.query.emoji}&title=${request.query.title}`).then(res => res.json()).then((res => {
       response.status(200).send(res);
     }))
   } else {
+    console.log(`Provided: ${providedToken}, expected: ${process.env.AUTH_TOKEN}`);
     response.status(403).send({result: "error", message: "Not allowed. Token invalid."});
   }
 }
