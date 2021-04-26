0908442038fc06b5d5897a3dd861e33a4e67dfd0
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-26 18:19:21 +0200

    Token auth

--- pages/admin.js
+++ pages/admin.js
@@ -1,19 +1,38 @@
 import Image from "next/image";
 import { PrimaryButton } from '@fluentui/react';
 
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 export default function AboutPage() {
     const [location, setLocation] = useState("Home");
     const [activity, setActivity] = useState("");
     const [emoji, setEmoji] = useState("");
+
+    const [token, setToken] = useState('');
     
     const sendActivity = () => {
-        fetch(encodeURI(`/api/update?title=${encodeURI(activity)}&emoji=${encodeURI(emoji)}&location=${encodeURI(location)}`)).then(res => res.json()).then(res => {
+        fetch(encodeURI(`/api/update?title=${encodeURI(activity)}&emoji=${encodeURI(emoji)}&location=${encodeURI(location)}&token=`)).then(res => res.json()).then(res => {
             console.log(res);
         });
+    }; 
+
+    useEffect(() => {
+        if (!token) {
+            setToken(localStorage.getItem("token"));
+            if (!localStorage.getItem("token")) {
+                const tokenNow = prompt("What's the secret password?");
+                setToken(tokenNow);
+                localStorage.setItem('token', tokenNow);
+            }
+        }
+    });
+
+    const onChange = event => {
+      localStorage.setItem('token', event.target.value);
+   
+      setToken(event.target.value);
     };
-  
+
     return (
         <div className="grid md:grid-cols-2 gap-6 grid-cols-1">
             <div>
--- pages/api/update.ts
+++ pages/api/update.ts
@@ -1,11 +1,18 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
+const sha256 = require('js-sha256');
+
 require('dotenv').config();
 
 export default (request: VercelRequest, response: VercelResponse) : void => {
-  const endpoint = process.env.SET_URL;
-  fetch(`${endpoint}?location=${request.query.location}&emoji=${request.query.emoji}&title=${request.query.title}`).then(res => res.json()).then((res => {
-    response.status(200).send(res);
-  }))
+  const providedToken = request.query.token;
+  if (process.env.TOKEN === sha256(providedToken)) {
+    const endpoint = process.env.SET_URL;
+    fetch(`${endpoint}?location=${request.query.location}&emoji=${request.query.emoji}&title=${request.query.title}`).then(res => res.json()).then((res => {
+      response.status(200).send(res);
+    }))
+  } else {
+    response.status(403).send("Not allowed. Token invalid.")
+  }
 }
